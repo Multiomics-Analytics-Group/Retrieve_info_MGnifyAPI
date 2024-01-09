fe77e10fdc928683f8de30c3660bc007bd910282
sayalaruano <sebasar1245@gmail.com>
2024-01-09 02:46:32 +0100

    Modify preprocessing scripts to handle differnt file formats

--- Scripts/Functions_preprocessing.py
+++ Scripts/Functions_preprocessing.py
@@ -56,32 +56,90 @@ def preprocess_abund_table_phylum(abund_table):
     
     return abund_table
 
-# Function to preprocess abundance table for a specific study and taxonomic rank
+# Functions to preprocess abundance table for a specific study and taxonomic rank
 def preprocess_abund_table(abund_table, tax_rank):
-    # Split the taxonomic information into separate columns, and drop the first column, 
-    # which contains just the Root word (not a taxonomic rank)
-    taxonomic_columns = abund_table[abund_table.columns[0]].str.split(';', expand=True).drop(columns=0)
+    # Determine the file format and call the respective function
+    if abund_table.iloc[0, 0].startswith('sk__') or str(abund_table.iloc[0, 0]) == 'Unclassified':
+        return preprocess_abund_table_superkingdom(abund_table, tax_rank)
+    elif abund_table.iloc[0, 0] == 'Root':
+        return preprocess_abund_table_root(abund_table, tax_rank)
+    else:
+        raise ValueError("Unknown file format")
 
-    # Rename the new columns as per the taxonomic ranks
-    taxonomic_columns.columns = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
+# Function to preprocess file format with Root as the first column
+def preprocess_abund_table_root(abund_table, tax_rank):
+    # Splitting taxonomic information
+    taxonomic_df = abund_table[abund_table.columns[0]].str.split(';', expand=True).drop(columns=0)
 
-    # Join the new taxonomic columns with the original abundance data, assuming 
-    # the abundance data starts from the second column
-    abund_table = pd.concat([taxonomic_columns, abund_table.iloc[:, 1:]], axis=1)
+    # Remove the first row corresponding to the Root taxonomic rank
+    taxonomic_df = taxonomic_df.iloc[1:, :]
+
+    # Define the column names for this format
+    taxonomic_df.columns = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
+    
+    # Isolating abundance data
+    abundance_df = abund_table.iloc[:, 1:]
 
     # Remove the first row corresponding to the sums of the counts in each column
-    abund_table = abund_table.iloc[1:, :]
+    abundance_df = abundance_df.iloc[1:, :]
 
-    # Delete additional characters (e.g. 'k__', 'p__', or '[]') from the taxonomic columns
-    for col in abund_table.columns[:7]:  # Assuming the first 7 columns are taxonomic ranks
-        abund_table[col] = abund_table[col].apply(lambda x: x[3:] if pd.notnull(x) else x)
-        abund_table[col] = abund_table[col].str.replace('[', '').str.replace(']', '')
+    # Delete additional characters (e.g. 'k__', 'p__', or '[]') from the taxonomic df
+    for col in taxonomic_df.columns:  
+        taxonomic_df[col] = taxonomic_df[col].apply(lambda x: x[3:] if pd.notnull(x) else x)
+        taxonomic_df[col] = taxonomic_df[col].str.replace('[', '').str.replace(']', '')
 
+    # Merge taxonomic and abundance dataframes
+    merged_df = pd.concat([taxonomic_df, abundance_df], axis=1)
+
+    # Filter out unwanted rows
+    filtered_df = filter_rows(merged_df, tax_rank)
+
+    # Aggregate data
+    aggregated_abund_table = aggregate_data(filtered_df, tax_rank)
+
+    return aggregated_abund_table
+
+# Function to preprocess file format with sk__ as the first column
+def preprocess_abund_table_superkingdom(abund_table, tax_rank):
+    # Split the taxonomic information into separate columns
+    taxonomic_df = abund_table[abund_table.columns[0]].str.split(';', expand=True)
+
+    # Define the column names for this format
+    taxonomic_df.columns  = ['Superkingdom', 'Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
+
+    # Remove the first row corresponding to the Root taxonomic rank
+    taxonomic_df = taxonomic_df.iloc[1:, :]
+    
+    # Isolating abundance data
+    abundance_df = abund_table.iloc[:, 1:]
+
+    # Remove the first row corresponding to the sums of the counts in each column
+    abundance_df = abundance_df.iloc[1:, :]
+
+    # Delete additional characters (e.g. 'k__', 'p__', 'sk__', or '[]') from the taxonomic df
+    for col in taxonomic_df.columns:
+        # Using .loc for safe modification
+        taxonomic_df.loc[:, col] = taxonomic_df[col].apply(lambda x: x.split("__", 1)[-1] if pd.notnull(x) else x)
+        taxonomic_df.loc[:, col] = taxonomic_df[col].str.replace('[', '').str.replace(']', '')
+
+    # Merge taxonomic and abundance dataframes
+    merged_df = pd.concat([taxonomic_df, abundance_df], axis=1)
+
+    # Filter out unwanted rows
+    filtered_df = filter_rows(merged_df, tax_rank)
+
+    # Aggregate data
+    aggregated_abund_table = aggregate_data(filtered_df, tax_rank)
+
+    return aggregated_abund_table
+
+# Function to filter out unwanted rows
+def filter_rows(abund_table, tax_rank):
     # Delete rows with NAN value in the tax_rank column
     abund_table = abund_table.dropna(subset=[tax_rank])
 
     # Delete rows with empty strings, None, or whitespace in the tax_rank column
-    abund_table[tax_rank] = abund_table[tax_rank].fillna('')  # Replace NaN with empty string
+    abund_table.loc[:, tax_rank] = abund_table[tax_rank].fillna('')  # Replace NaN with empty string
     abund_table = abund_table[~abund_table[tax_rank].apply(lambda x: not x.strip())]  # Filter out empty strings
 
     # Delete rows with unassigned and unclassified taxkanomic rank
@@ -101,20 +159,21 @@ def preprocess_abund_table(abund_table, tax_rank):
     # Filter out rows where the tax_rank is all in lowercase
     abund_table = abund_table[~abund_table[tax_rank].str.islower()]
 
+    # Replace None values with empty strings
+    abund_table = abund_table.fillna('')
+
     # Reset index
     abund_table = abund_table.reset_index(drop=True)
 
-    # Identify the columns for aggregation (abundance data) and the taxonomic columns
-    tax_column_names = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
+    return abund_table
 
+# Function to aggregate data
+def aggregate_data(abund_table, tax_rank):
     # Find the index of the tax_rank in the taxonomic_columns list
-    tax_rank_index = tax_column_names.index(tax_rank)
+    tax_rank_index = abund_table.columns.tolist().index(tax_rank)
 
     # Group by the taxonomic ranks up to and including the tax_rank
-    groupby_columns = tax_column_names[:tax_rank_index + 1]
-    aggregated_abund_table = abund_table.groupby(groupby_columns).sum()
+    groupby_columns = abund_table.columns[:tax_rank_index + 1].tolist()
+    aggregated_data = abund_table.groupby(groupby_columns).sum()
 
-    # Reset index to turn the groupby columns back into columns in the DataFrame
-    aggregated_abund_table = aggregated_abund_table.reset_index()
-
-    return aggregated_abund_table
+    return aggregated_data.reset_index()
