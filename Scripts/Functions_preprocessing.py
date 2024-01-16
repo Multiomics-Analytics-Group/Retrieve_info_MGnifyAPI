4e369ae9d4852531228c247626d85f4dde5ec16e
sayalaruano <sebasar1245@gmail.com>
2024-01-16 20:44:10 +0100

    Add function to reove duplicated analyses for the same samples

--- Scripts/Functions_preprocessing.py
+++ Scripts/Functions_preprocessing.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import numpy as np
+import random
 import glob
 import os
 
 def load_abund_table(folder_path, selected_study, phylum):
+    """
+    Load the abundance table for a specific study and taxonomic rank
+    Input: folder_path (str) - path to the folder containing the abundance table
+           selected_study (str) - study accession number
+           phylum (bool) - True if the taxonomic rank is phylum, False otherwise
+    Output: abund_table (DataFrame) - DataFrame with the abundance table for the study and taxonomic rank
+    """
     # Broad pattern to initially match files
     broad_pattern = f"{selected_study}*taxonomy*.tsv"
     file_list = glob.glob(os.path.join(folder_path, broad_pattern))
@@ -30,6 +38,11 @@ def load_abund_table(folder_path, selected_study, phylum):
 
 # Function to preprocess phylum abundance table for a specific study
 def preprocess_abund_table_phylum(abund_table):
+    """
+    Preprocess the abundance table at phylum lvel for a specific study
+    Input: abund_table (DataFrame) - DataFrame with the abundance table for the study
+    Output: abund_table (DataFrame) - DataFrame with the abundance table for the study after preprocessing
+    """
     # Delete rows with NAN value in the 'phylum' column
     abund_table = abund_table.dropna(subset=['phylum'])
 
@@ -63,6 +76,12 @@ def preprocess_abund_table_phylum(abund_table):
 
 # Functions to preprocess abundance table for a specific study and taxonomic rank
 def preprocess_abund_table(abund_table, tax_rank):
+    """
+    Preprocess the abundance table for a specific study and taxonomic rank
+    Input: abund_table (DataFrame) - DataFrame with the abundance table for the study
+           tax_rank (str) - taxonomic rank to preprocess
+    Output: abund_table (DataFrame) - DataFrame with the abundance table for the study after preprocessing
+    """
     # Determine the file format and call the respective function
     if abund_table.iloc[0, 0].startswith('sk__') or str(abund_table.iloc[0, 0]) == 'Unclassified':
         return preprocess_abund_table_superkingdom(abund_table, tax_rank)
@@ -73,6 +92,12 @@ def preprocess_abund_table(abund_table, tax_rank):
 
 # Function to preprocess file format with Root as the first column
 def preprocess_abund_table_root(abund_table, tax_rank):
+    """
+    Preprocess the abundance table for a specific study and taxonomic rank in the file format with Root as the first column
+    Input: abund_table (DataFrame) - DataFrame with the abundance table for the study
+           tax_rank (str) - taxonomic rank to preprocess
+    Output: abund_table (DataFrame) - DataFrame with the abundance table for the study after preprocessing
+    """
     # Splitting taxonomic information
     taxonomic_df = abund_table[abund_table.columns[0]].str.split(';', expand=True).drop(columns=0)
 
@@ -109,6 +134,12 @@ def preprocess_abund_table_root(abund_table, tax_rank):
 
 # Function to preprocess file format with sk__ as the first column
 def preprocess_abund_table_superkingdom(abund_table, tax_rank):
+    """
+    Preprocess the abundance table for a specific study and taxonomic rank in the file format with sk__ as the first column
+    Input: abund_table (DataFrame) - DataFrame with the abundance table for the study
+           tax_rank (str) - taxonomic rank to preprocess
+    Output: abund_table (DataFrame) - DataFrame with the abundance table for the study after preprocessing
+    """
     # Split the taxonomic information into separate columns
     taxonomic_df = abund_table[abund_table.columns[0]].str.split(';', expand=True)
 
@@ -154,6 +185,12 @@ def preprocess_abund_table_superkingdom(abund_table, tax_rank):
 
 # Function to filter out unwanted rows
 def filter_rows(abund_table, tax_rank):
+    """
+    Filter out unwanted rows from the abundance table
+    Input: abund_table (DataFrame) - DataFrame with the abundance table for the study
+           tax_rank (str) - taxonomic rank to preprocess
+    Output: abund_table (DataFrame) - DataFrame with the abundance table for the study after filtering
+    """
     # Delete rows with NAN value in the tax_rank column
     abund_table = abund_table.dropna(subset=[tax_rank])
 
@@ -188,6 +225,12 @@ def filter_rows(abund_table, tax_rank):
 
 # Function to aggregate data
 def aggregate_data(abund_table, tax_rank):
+    """
+    Aggregate the abundance table by grouping by the taxonomic ranks up to and including the tax_rank
+    Input: abund_table (DataFrame) - DataFrame with the abundance table for the study
+           tax_rank (str) - taxonomic rank to preprocess
+    Output: aggregated_data (DataFrame) - DataFrame with the abundance table for the study after aggregation
+    """
     # Find the index of the tax_rank in the taxonomic_columns list
     tax_rank_index = abund_table.columns.tolist().index(tax_rank)
 
@@ -198,6 +241,12 @@ def aggregate_data(abund_table, tax_rank):
     return aggregated_data.reset_index()
 
 def rename_duplicated_taxa(abund_table, tax_rank):
+    """
+    Rename duplicated taxa in the abundance table, prefixing the name with the first four letters of the higher taxonomic rank
+    Input: abund_table (DataFrame) - DataFrame with the abundance table for the study
+           tax_rank (str) - taxonomic rank to preprocess
+    Output: abund_table (DataFrame) - DataFrame with the abundance table for the study after renaming duplicated taxa
+    """
     # Check duplicated rows at the Genus level
     duplicated_genus = abund_table[abund_table.duplicated(subset=tax_rank, keep=False)]
 
@@ -215,3 +264,50 @@ def rename_duplicated_taxa(abund_table, tax_rank):
         abund_table.at[index, tax_rank] = new_genus_name
 
     return abund_table
+
+def drop_duplicatedsamples(abundance_df, metadata_df, phylum):
+    """
+    Preprocess metagenomics data by ensuring only one analysis per sample is retained in the abundance table.
+    Ensures that the selected analysis ID is present in the abundance table.
+    Input: metadata_df (DataFrame) - DataFrame with the metadata for the study
+           abundance_df (DataFrame) - DataFrame with the abundance table for the study
+           phylum (bool) - True if the taxonomic rank is phylum, False otherwise
+    Output: filtered_abundance_df (DataFrame) - DataFrame with the abundance table for the study after filtering
+            missing_analysis_samples (list) - list of samples that do not have any analysis IDs present in the abundance table
+            dropped_analysis_ids (dict) - dictionary with the sample IDs as keys and the dropped analysis IDs as values
+    """
+    selected_analysis_ids = {}
+    missing_analysis_samples = []
+    dropped_analysis_data = []  # List to store dropped analysis data
+
+    if phylum:
+        taxonomic_columns = ['kingdom', 'phylum']
+    else:
+        possible_taxonomic_columns = ['Superkingdom', 'Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
+        taxonomic_columns = [col for col in possible_taxonomic_columns if col in abundance_df.columns]
+
+    for _, row in metadata_df.iterrows():
+        if pd.isna(row['assembly_run_ids']):
+            missing_analysis_samples.append(row['sample_id'])
+            continue
+
+        analysis_ids = row['assembly_run_ids'].split(';')
+        valid_analysis_ids = [id for id in analysis_ids if id in abundance_df.columns]
+
+        if valid_analysis_ids:
+            selected_id = random.choice(valid_analysis_ids)
+            selected_analysis_ids[row['sample_id']] = selected_id
+            # Collect the IDs that are available but not chosen
+            dropped_ids = set(valid_analysis_ids) - set([selected_id])
+            for dropped_id in dropped_ids:
+                dropped_analysis_data.append({'sample_id': row['sample_id'], 'dropped_analysis_id': dropped_id})
+        else:
+            missing_analysis_samples.append(row['sample_id'])
+
+    filtered_columns = taxonomic_columns + list(selected_analysis_ids.values())
+    filtered_abundance_df = abundance_df[filtered_columns]
+
+    # Convert the list of dropped analysis data to a DataFrame
+    dropped_analysis_df = pd.DataFrame(dropped_analysis_data)
+
+    return filtered_abundance_df, missing_analysis_samples, dropped_analysis_df
